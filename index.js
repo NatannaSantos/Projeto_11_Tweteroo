4199fb50280e7bfda4f709d92cecace35b7588e9
Natanna <natannacortez@gmail.com>
2022-01-24 12:30:51 -0300

    acrescimo avatar aos tweets

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ server.use(json());
 
 const users = [];
 const tweets=[];
+const listTweets=[];
+
 
 server.post('/sign-up', (req, res) => {
     const user = req.body;   
@@ -20,20 +22,38 @@ server.get('/sign-up',(req,res)=>{
 });
 
 server.post('/tweets',(req,res)=>{
-    const tweet=req.body;
-    tweets.push(tweet);
+    
+    
+    const tweet=req.body; 
+    
+    let avatar='';   
+    tweets.push(tweet);    
+    for(let i=0;i<users.length;i++){   
+        for(let j=0;j<tweets.length;j++){
+            if(users[i].username===tweets[j].username){
+                avatar=users[i].avatar
+            }
+        }  
+    }                  
+      
+    const newTweet = {
+      ...tweet,
+      "avatar": avatar
+    }
+    listTweets.push(newTweet);
+    
     res.send("ok");
 });
-server.get('/tweets',(req,res)=>{
-    if(tweets.length < 10){
-        res.send(tweets);
+server.get('/tweets',(req,res)=>{    
+    if(listTweets.length < 10){
+        res.send(listTweets);
       }else{
-        res.send(tweets.slice((tweets.length - 10), tweets.length));
+        res.send(listTweets.slice((listTweets.length - 10), listTweets.length));
       }
 });
 
 server.listen(5000,()=>{
-    console.log("Rodando em http://localhost:5000")
+    console.log("Rodando em http://localhost:5000");
 });
 
 
